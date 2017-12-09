446c20bac0a76f4868a06434a5bbd7c5b49b73af
brad <brad@brad.tw>
2017-12-09 09:43:20 +0800

    201712090943

--- src/test01.js
+++ src/test01.js
@@ -5,6 +5,7 @@ var Test01Layer = cc.Layer.extend({
     moveToDown: null,
     moveToUp: null,
     moveToLeft: null,
+    dir: 0,     // 1:right; 2:down; 3:left; 4:up
     ctor:function () {
         this._super();
 
@@ -18,9 +19,10 @@ var Test01Layer = cc.Layer.extend({
                 cc.winSize.width-this.sprite.width/2,
                 this.sprite.y));
         this.sprite.runAction(this.moveToRight);
+        this.dir = 1;
 
         this.moveToRight = cc.moveTo(1,
-            cc.p(cc.winSize.width/2,
+            cc.p(cc.winSize.width - this.sprite.width/2,
                 cc.winSize.height - this.sprite.height/2));
         this.moveToDown = cc.moveTo(1,
             cc.p(cc.winSize.width-this.sprite.width/2,
@@ -35,15 +37,30 @@ var Test01Layer = cc.Layer.extend({
         this.sprite.schedule(
             this.spriteTask,1,cc.REPEAT_FOREVER,1);
 
-
-
-
-
-
         return true;
     },
 
     spriteTask: function () {
+        var layer = this.getParent();
+        switch (layer.dir){
+            case 1:
+                this.runAction(layer.moveToDown);
+                layer.dir = 2;
+                break;
+            case 2:
+                this.runAction(layer.moveToLeft);
+                layer.dir = 3;
+                break;
+            case 3:
+                this.runAction(layer.moveToUp);
+                layer.dir = 4;
+                break;
+            case 4:
+                this.runAction(layer.moveToRight);
+                layer.dir = 1;
+                break;
+        }
+
 
     }
 
