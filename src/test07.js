8ed5a109bdca9898d37d9d60980a5ba1501b0b30
brad <brad@brad.tw>
2017-12-09 14:27:45 +0800

    201712091427

--- src/test07.js
+++ src/test07.js
@@ -4,43 +4,50 @@ var Test07Layer = cc.Layer.extend({
     ctor:function () {
         this._super();
 
-        var label = new cc.LabelTTF("", "Arial", 42,
-            cc.size(300,50), cc.TEXT_ALIGNMENT_CENTER);
-        label.x = cc.winSize.width/2;
-        label.y = cc.winSize.height/2;
-        this.addChild(label);
+        // var label = new cc.LabelTTF("", "Arial", 42,
+        //     cc.size(300,50), cc.TEXT_ALIGNMENT_CENTER);
+        // label.x = cc.winSize.width/2;
+        // label.y = cc.winSize.height/2;
+        // this.addChild(label);
 
-        var baseData = cc.sys.localStorage.getItem("baseData");
-        cc.log("before =>" + baseData);
+        this.sprite = new cc.Sprite(res.HelloWorld_png);
+        //this.sprite.x = cc.winSize.width/2 ;
+        //this.sprite.y = cc.winSize.height/2 ;
+        this.addChild(this.sprite);
+
+
+        var baseData = cc.sys.localStorage.getItem("gameData4");
         baseData = JSON.parse(baseData);
-        cc.log("after =>" + baseData);
         if (baseData){
             // 有資料
             cc.log('OK');
-            label.setString("Got Data");
+            //label.setString("Got Data");
 
             // fetch data
-            cc.log(baseData.username);
-            cc.log(baseData.sound);
-            cc.log(baseData.stage);
-            cc.log(baseData.blood);
+            //this.sprite.x = baseData.sprite.x;
+            //this.sprite.y = baseData.sprite.y;
 
+            cc.log(baseData.datas[1].x);
 
+            //cc.sys.localStorage.removeItem("baseData");
 
         }else{
             // 沒資料
             cc.log('xx');
-            label.setString("No Data");
+            //label.setString("No Data");
+
+            this.sprite.x = this.sprite.width/2 + Math.random()*800;
+            this.sprite.y = this.sprite.height/2 + Math.random()*400;
 
             var newData = {
-              username: 'Brad',
-              sound: true,
-              stage: 3,
-              blood: 4.7
+                datas: [1,{"x":12,"y":13},3,4],
+                //obj: this.sprite
             };
+
             var data = JSON.stringify(newData);
+
             cc.log(data);
-            cc.sys.localStorage.setItem("baseData", data);
+            cc.sys.localStorage.setItem("gameData4", data);
 
 
         }
