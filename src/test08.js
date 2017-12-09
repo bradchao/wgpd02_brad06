d800c7fbdb0d1570ce486767b4618acb7fe15917
brad <brad@brad.tw>
2017-12-09 15:27:05 +0800

    201712091526

--- src/test08.js
+++ src/test08.js
@@ -9,7 +9,14 @@ var Test08Layer = cc.Layer.extend({
             this.onClick, this);
         play.x = 0;
         play.y = 0;
-        var menu = new cc.Menu(play);
+
+        var play1 = new cc.MenuItemImage(res.HelloWorld_png,
+            res.HelloWorld_png, res.HelloWorld_png,
+            this.onClick1, this);
+        play1.x = 200;
+        play1.y = 0;
+
+        var menu = new cc.Menu(play, play1);
         this.addChild(menu);
 
         cc.audioEngine.playMusic(res.Chop, true);
@@ -21,7 +28,16 @@ var Test08Layer = cc.Layer.extend({
         cc.audioEngine.playEffect(res.Click);
         cc.audioEngine.stopMusic(true);
 
-    }
+    },
+    onClick1: function () {
+        cc.audioEngine.playEffect("res/r_jump.ogg");
+        //cc.audioEngine.stopMusic(true);
+
+        //cc.audioEngine.stopAllEffects();
+        //cc.audioEngine.end();
+
+
+    },
 });
 
 var Test08Scene = cc.Scene.extend({
